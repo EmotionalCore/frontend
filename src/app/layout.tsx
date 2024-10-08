98f0f85e01c4f60105914059df97345817eadaa4
박지원 <jackgg12322@gmail.com>
2024-10-08 17:49:05 +0900

    Fix: ".prettierrc 세미콜론 사용 여부 활성화"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,29 @@
-import type { Metadata } from 'next'
-import localFont from 'next/font/local'
-import './globals.css'
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import './globals.css';
 
 const pretendard = localFont({
   src: './fonts/PretendardVariable.woff2',
   display: 'swap',
   weight: '45 920',
   variable: '--font-pretendard',
-})
+});
 
 const LaundryGothicOTF = localFont({
   src: './fonts/런드리고딕 Regular.woff',
   display: 'swap',
   variable: '--font-laundry',
-})
+});
 
 export const metadata: Metadata = {
   title: '감성코어',
   description: '시 및 소설 웹 플랫폼입니다.',
-}
+};
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang='kr' className={`${pretendard.variable} ${LaundryGothicOTF.variable}`}>
       <body className={`${pretendard.className} `}>{children}</body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,5 @@ export default function Home() {
     <div className='flex items-center justify-center'>
       <p className='text-18-700 text-blue-9'>안녕하세요</p>
     </div>
-  )
+  );
 }
