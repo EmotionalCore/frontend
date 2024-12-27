6e97e94a9cd332292ef18f64ab1cebfec7a5d1c4
박지원 <jackgg12322@gmail.com>
2024-12-27 23:59:20 +0900

    Chore:"불필요 선언 제거"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import Headers from './components/_common/Headers';
-
 export default function Home() {
   return (
     <main>
-      <Headers />
       <div className='flex items-center justify-center'>
         <p className='text-18-700 text-blue-9'>안녕하세요</p>
       </div>
