34c4991b185c760f5c872ff6abebfceee99766ea
박지원 <jackgg12322@gmail.com>
2024-12-27 23:59:20 +0900

    Chore:"불필요 선언 제거"

--- src/app/(sub pages)/layout.tsx
+++ src/app/(sub pages)/layout.tsx
@@ -1,14 +1,9 @@
 import { ReactNode } from 'react';
-import Headers from '../components/_common/Headers';
 
 interface SubPagesLayoutProps {
   children: ReactNode;
 }
+
 export default function SubPagesLayout({ children }: SubPagesLayoutProps) {
-  return (
-    <main className='flex flex-col items-center justify-center'>
-      <Headers />
-      {children}
-    </main>
-  );
+  return <main className='flex flex-col items-center justify-center'>{children}</main>;
 }
