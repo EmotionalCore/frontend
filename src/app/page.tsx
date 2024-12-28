6abfc0da5937c53da7386a49d3174dce2938218c
박지원 <jackgg12322@gmail.com>
2024-12-28 18:41:06 +0900

    Chore:"tailwind Prettier error 해결"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <div className='flex items-center justify-center'>
-        <p className='font-SCDream1 text-18-700 text-blue-9'>안녕하세요</p>
+        <p className='text-blue-9 font-SCDream1 text-18-700'>안녕하세요</p>
       </div>
     </main>
   );
