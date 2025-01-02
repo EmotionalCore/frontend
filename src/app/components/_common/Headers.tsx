58d9b2a008d18fb9a5e7fd3763887021772be119
SJ-1220 <kindjin12@naver.com>
2025-01-02 20:35:42 +0900

    Chore: "rem값 수정"

--- src/app/components/_common/Headers.tsx
+++ src/app/components/_common/Headers.tsx
@@ -2,31 +2,37 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 
-const Headers = () => {
+const Headers: React.FC = () => {
   return (
-    <div className='flex h-[3.9375rem] items-center bg-gray-200 p-[1rem] font-SCDream5 text-[1.125rem] text-gray-900'>
-      <div className='ml-[22.5rem] text-[1.5rem] text-black-0'>감성코어</div>
-      <div>
-        <button className='ml-[2.9rem]'>홈</button>
-      </div>
-      <div>
-        <button className='ml-[2.12rem]'>게시판</button>
-      </div>
-      <div>
-        <button className='ml-[2.12rem]'>검색</button>
-      </div>
-      <div>
-        <button className='ml-[2.12rem]'>서재</button>
-      </div>
-      <button className='ml-[38.81rem] flex items-center'>
-        <div className='relative h-[0.875rem] w-[0.82638rem]'>
-          <Image src='/image/add_icon.svg' alt='add_icon' layout='fill' />
+    <div className='flex h-[6.3rem] items-center bg-gray-200 font-SCDream5 text-[1.8rem] text-gray-900'>
+      <div className='ml-[36rem] text-[2.4rem] text-black-0'>감성코어</div>
+      <Link href='/' passHref>
+        <button className='ml-[4.3rem]'>홈</button>
+      </Link>
+      <Link href='/board' passHref>
+        <button className='ml-[3.4rem]'>게시판</button>
+      </Link>
+      <Link href='/search' passHref>
+        <button className='ml-[3.4rem]'>검색</button>
+      </Link>
+      <Link href='/library' passHref>
+        <div>
+          <button className='ml-[3.4rem]'>서재</button>
         </div>
-        <div className='ml-[0.44rem] text-[#067DFD]'>작품등록</div>
-      </button>
-      <button className='relative ml-[0.45rem] h-[2.63394rem] w-[2.5rem]' title='none_user'>
-        <Image src='/image/none_user.png.svg' alt='none_user' layout='fill' />
-      </button>
+      </Link>
+      <Link href='/forum/post' passHref>
+        <button className='ml-[51.58rem] flex items-center'>
+          <div className='relative h-[1.4rem] w-[1.3222rem]'>
+            <Image src='/image/add_icon.svg' alt='add_icon' layout='fill' />
+          </div>
+          <div className='ml-[0.7rem] text-[#067DFD]'>작품등록</div>
+        </button>
+      </Link>
+      <Link href='/mypage' passHref>
+        <button className='relative ml-[0.71rem] h-[4.2143rem] w-[4rem]' title='none_user'>
+          <Image src='/image/none_user.png.svg' alt='none_user' layout='fill' />
+        </button>
+      </Link>
     </div>
   );
 };
