c9329923f291d6c7634908104164d0c2328f9caf
SJ-1220 <kindjin12@naver.com>
2025-01-02 21:26:50 +0900

    Feat:"경로 설정"

--- src/app/components/_common/Headers.tsx
+++ src/app/components/_common/Headers.tsx
@@ -6,33 +6,45 @@ const Headers: React.FC = () => {
   return (
     <div className='flex h-[6.3rem] items-center bg-gray-200 font-SCDream5 text-[1.8rem] text-gray-900'>
       <div className='ml-[36rem] text-[2.4rem] text-black-0'>감성코어</div>
-      <Link href='/' passHref>
-        <button className='ml-[4.3rem]'>홈</button>
-      </Link>
-      <Link href='/board' passHref>
-        <button className='ml-[3.4rem]'>게시판</button>
-      </Link>
-      <Link href='/search' passHref>
-        <button className='ml-[3.4rem]'>검색</button>
-      </Link>
-      <Link href='/library' passHref>
-        <div>
-          <button className='ml-[3.4rem]'>서재</button>
-        </div>
-      </Link>
-      <Link href='/forum/post' passHref>
-        <button className='ml-[51.58rem] flex items-center'>
-          <div className='relative h-[1.4rem] w-[1.3222rem]'>
-            <Image src='/image/add_icon.svg' alt='add_icon' layout='fill' />
+      <div className='ml-[4.3rem]'>
+        <Link href='/' passHref>
+          <button>홈</button>
+        </Link>
+      </div>
+      <div className='ml-[3.4rem]'>
+        <Link href='/board' passHref>
+          <button>게시판</button>
+        </Link>
+      </div>
+      <div className='ml-[3.4rem]'>
+        <Link href='/search' passHref>
+          <button>검색</button>
+        </Link>
+      </div>
+      <div className='ml-[3.4rem]'>
+        <Link href='/library' passHref>
+          <div>
+            <button>서재</button>
           </div>
-          <div className='ml-[0.7rem] text-[#067DFD]'>작품등록</div>
-        </button>
-      </Link>
-      <Link href='/mypage' passHref>
-        <button className='relative ml-[0.71rem] h-[4.2143rem] w-[4rem]' title='none_user'>
-          <Image src='/image/none_user.png.svg' alt='none_user' layout='fill' />
-        </button>
-      </Link>
+        </Link>
+      </div>
+      <div className='ml-[51.58rem]'>
+        <Link href='/forum/post' passHref>
+          <button className='flex items-center'>
+            <div className='relative h-[1.4rem] w-[1.3222rem]'>
+              <Image src='/image/add_icon.svg' alt='add_icon' layout='fill' />
+            </div>
+            <div className='ml-[0.7rem] text-[2.4rem] text-[#067DFD]'>작품등록</div>
+          </button>
+        </Link>
+      </div>
+      <div className='ml-[0.71rem]'>
+        <Link href='/mypage' passHref>
+          <button className='relative flex h-[4.2143rem] w-[4rem] items-center' title='none_user'>
+            <Image src='/image/none_user.png.svg' alt='none_user' layout='fill' />
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
