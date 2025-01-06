1a5745207140337806878a69adf862da34239c5e
SJ-1220 <kindjin12@naver.com>
2025-01-06 15:56:36 +0900

    Refactor: "이미지 적용 방식, Link 사용 방식 수정"

--- src/app/components/_common/Headers.tsx
+++ src/app/components/_common/Headers.tsx
@@ -1,38 +1,38 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import NoneUserIcon from '/public/image/none-user-icon.svg';
+import AddIcon from '/public/image/add-icon.svg';
 
-const Headers: React.FC = () => {
+const Headers = () => {
   return (
-    <div className='flex h-[6.3rem] items-center bg-gray-200 font-SCDream5 text-[1.8rem] text-gray-900'>
+    <div className='flex h-[6.3rem] items-center bg-white-F font-SCDream5 text-[1.8rem] text-gray-900'>
       <div className='ml-[36rem] text-[2.4rem] text-black-0'>감성코어</div>
       <div className='ml-[4.3rem]'>
         <Link href='/' passHref>
-          <div>홈</div>
+          홈
         </Link>
       </div>
       <div className='ml-[3.4rem]'>
         <Link href='/board' passHref>
-          <div>게시판</div>
+          게시판
         </Link>
       </div>
       <div className='ml-[3.4rem]'>
         <Link href='/search' passHref>
-          <div>검색</div>
+          검색
         </Link>
       </div>
       <div className='ml-[3.4rem]'>
         <Link href='/library' passHref>
-          <div>
-            <div>서재</div>
-          </div>
+          서재
         </Link>
       </div>
       <div className='ml-[51.58rem]'>
         <Link href='/forum/post' passHref>
           <div className='flex items-center'>
             <div className='relative h-[1.4rem] w-[1.3222rem]'>
-              <Image src='/image/add-icon.svg' alt='add_icon' layout='fill' />
+              <Image src={AddIcon} alt='add icon' layout='fill' />
             </div>
             <div className='ml-[0.7rem] text-[2.4rem] text-[#067DFD]'>작품등록</div>
           </div>
@@ -41,7 +41,7 @@ const Headers: React.FC = () => {
       <div className='ml-[0.71rem]'>
         <Link href='/mypage' passHref>
           <div className='relative flex h-[4.2143rem] w-[4rem] items-center' title='none_user'>
-            <Image src='/image/none-user.svg' alt='none_user' layout='fill' />
+            <Image src={NoneUserIcon} alt='none user icon' layout='fill' />
           </div>
         </Link>
       </div>
