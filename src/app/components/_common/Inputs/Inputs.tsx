188ed0ef56517549b955da0af7a3d034ed987ad2
SJ-1220 <kindjin12@naver.com>
2025-01-14 19:26:39 +0900

    Chore: "placeholder 속성 삭제"

--- src/app/components/_common/Inputs/Inputs.tsx
+++ src/app/components/_common/Inputs/Inputs.tsx
@@ -4,18 +4,17 @@ import { UseFormRegisterReturn, FieldErrors } from 'react-hook-form';
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   name: string;
   type: 'text' | 'password' | 'email' | 'checkbox' | 'search';
-  placeholder?: string;
   className?: string;
   label: string;
   register: UseFormRegisterReturn;
   errors: FieldErrors;
 }
 
-export default function Inputs({ name, type, placeholder, className, label, register, errors, ...props }: InputProps) {
+export default function Inputs({ name, type, className, label, register, errors, ...props }: InputProps) {
   return (
     <div>
       <label htmlFor={name}>{label}</label>
-      <input id={name} {...register} type={type} placeholder={placeholder} className={className} {...props} />
+      <input id={name} type={type} className={className} {...register} {...props} />
       {errors[name]?.message && <span>{errors[name].message as string}</span>}
     </div>
   );
