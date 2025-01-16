ddba09873d3e3fe4673bf412292f352ed3b015f5
SJ-1220 <kindjin12@naver.com>
2025-01-16 15:18:28 +0900

    Feat: "input의 type을 따로 정의"

---
+++ src/app/components/_common/Inputs/types.ts
@@ -0,0 +1,10 @@
+import { UseFormRegisterReturn, FieldErrors } from 'react-hook-form';
+
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  type: 'text' | 'password' | 'email' | 'checkbox' | 'search';
+  className?: string;
+  label: string;
+  register: UseFormRegisterReturn;
+  errors: FieldErrors;
+}
