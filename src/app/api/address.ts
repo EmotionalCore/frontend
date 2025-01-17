cd3b57ee1a2b37e7270195b4c333cd2fcd6706a1
박지원 <jackgg12322@gmail.com>
2025-01-17 13:21:44 +0900

    Setting:"api address 설정(address.ts)"

--- src/app/api/address.ts
+++ src/app/api/address.ts
@@ -1,4 +1,40 @@
+const memberBaseUrl = '/api/member/';
+const worksBaseUrl = '/api/work/';
+
+//유저 API
 export const signAddress = {
-  signIn: '/api/member/signin',
-  signUp: '/api/member/signup',
+  signIn: `${memberBaseUrl}signin`,
+  signUp: `${memberBaseUrl}signup`,
+};
+
+//소셜 로그인 API
+export const oauthAddress = {
+  naver: '/signin/naver',
+  google: '/login/oauth2/code/{registrationId}',
+  kakako: '/auth/oauth2/kakao',
+};
+
+//작품 API
+export const worksAddress = {
+  search: {
+    default: `${worksBaseUrl}search`,
+    popular: `${worksBaseUrl}popular`,
+  },
+
+  recommend: {
+    webtoon: `${worksBaseUrl}webtoon`,
+    poem: `${worksBaseUrl}poem`,
+    novel: `${worksBaseUrl}novel`,
+  },
+
+  new: {
+    default: `${worksBaseUrl}new`,
+    author: `${worksBaseUrl}author`,
+  },
+  all: `${worksBaseUrl}all`,
+  best: `${worksBaseUrl}best/today`,
+  authorBest: `${worksBaseUrl}author/best/monthly`,
+  popular: `${worksBaseUrl}popular/monthly`,
+  type: `${worksBaseUrl}type`,
+  tag: `${worksBaseUrl}tag`,
 };
