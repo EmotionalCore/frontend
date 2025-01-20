6184df5b23062136d4ab0f3600caef70482f0ae4
박지원 <jackgg12322@gmail.com>
2025-01-20 16:46:39 +0900

    Chore:"main page reset"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,7 @@
-//Dropdown 사용 예시
-
-'use client';
-import DropdownMenu, { MenuItemProps } from './components/_common/DropdownMenu';
-import DeleteIcon from '../../public/image/delete-icon.svg';
-import ShareIcon from '../../public/image/share-icon.svg';
-import EditIcon from '../../public/image/edit-icon.svg';
-const items: MenuItemProps[] = [
-  {
-    id: '1',
-    label: '수정하기',
-    icon: ShareIcon,
-    alt: 'edit',
-    color: 'default',
-    onClick: () => {
-      console.log('수정하기 클릭됨');
-    },
-  },
-  {
-    id: '2',
-    label: '공유하기',
-    icon: EditIcon,
-    alt: 'share',
-    color: 'default',
-    onClick: () => {
-      console.log('공유하기 클릭됨');
-    },
-  },
-  {
-    id: '3',
-    label: '삭제하기',
-    icon: DeleteIcon,
-    alt: 'delete',
-    color: 'red',
-    onClick: () => {
-      console.log('삭제하기 클릭됨');
-    },
-  },
-];
-
 export default function Home() {
   return (
     <main>
-      <div className='flex items-center justify-center'>
-        <DropdownMenu items={items} size='default' variant='default' />
-      </div>
+      <div className='flex items-center justify-center'>메인페이지</div>
     </main>
   );
 }
