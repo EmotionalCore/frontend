8aa8a33d44b4c100023e41b1864f34941e9e5ff0
SJ-1220 <kindjin12@naver.com>
2025-02-04 19:43:07 +0900

    Chore: "password patter 수정"

--- src/app/_lib/zod/InputsValidation.ts
+++ src/app/_lib/zod/InputsValidation.ts
@@ -3,7 +3,7 @@ import z from 'zod';
 const dummyUsername = ['username1', 'username2', 'username3', 'username4'];
 const dummyEmail = ['q1@qq.qq', 'q2@qq.qq', 'q3@qq.qq', 'q4@qq.qq'];
 
-const passwordPattern = new RegExp(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).+$/);
+const passwordPattern = new RegExp(/^(?=.*?[A-Za-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).+$/);
 
 export const InputsValidation = z
   .object({
@@ -17,12 +17,8 @@ export const InputsValidation = z
       .toLowerCase()
       .email({ message: '이메일을 입력해주세요.' })
       .refine((value) => !dummyEmail.includes(value), { message: '이미 사용 중인 이메일입니다.' }),
-    password: z
-      .string()
-      .min(8, { message: '비밀번호를 입력해주세요.' })
-      .max(15)
-      .regex(passwordPattern, { message: '' }),
-    passwordConfirm: z.string().min(8, { message: '' }).max(15, { message: '' }),
+    password: z.string().min(8, { message: '비밀번호를 입력해주세요.' }).max(15).regex(passwordPattern),
+    passwordConfirm: z.string(),
   })
   .refine((value) => value.password == value.passwordConfirm, {
     message: '비밀번호가 틀립니다',
