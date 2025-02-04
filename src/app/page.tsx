a4aea06b19235f65132129abb03b71fc8de72f50
za0012 <zinzeek@gmail.com>
2025-02-04 20:22:36 +0900

    Feat: 버튼 컴포넌트 개발(타입 분리 및 tsx작성)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import Buttons from './components/_common/Buttons/Button';
 
 const page = () => {
-  return <main className='flex h-[100rem] flex-col items-center justify-center'>메인</main>;
+  return (
+    <main className='flex h-[100rem] flex-col items-center justify-center'>
+      메인
+      <Buttons intent='whiteBorder' size='xxs'>
+        태그 연습
+      </Buttons>
+      <br />
+      <Buttons intent='red' size='xs'>
+        태그 연습
+      </Buttons>
+      <br />
+      <Buttons intent='primary' size='sm'>
+        태그 연습
+      </Buttons>
+      <br />
+      <Buttons intent='skyblue' size='md'>
+        태그 연습
+      </Buttons>
+      <br />
+      <Buttons intent='primary' size='lg'>
+        확인
+      </Buttons>
+      <br />
+      <Buttons intent='green' size='mdLogIn'>
+        태그 연습
+      </Buttons>
+    </main>
+  );
 };
 
 export default page;
