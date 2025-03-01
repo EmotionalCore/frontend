f0c42a901431832c657b5f56ba680df53d8170c1
박지원 <jackgg12322@gmail.com>
2025-03-01 13:10:56 +0900

    Refactor:"경로에 따른 조건부 페이지 타이틀 설정(Best.tsx)"

--- src/app/_components/domain/main/works/sections/best/Best.tsx
+++ src/app/_components/domain/main/works/sections/best/Best.tsx
@@ -2,16 +2,14 @@
 
 import { getBestWorksApi } from '@/api/works';
 import WorkSection from '../WorkSection';
+import { usePathname } from 'next/navigation';
 
 const Best = () => {
+  const pathname = usePathname();
+
+  const isPathNameTitle = pathname === '/' ? '감성코어 인기 best 작품 순위' : '';
   return (
-    <WorkSection
-      title='감성코어 인기 best 작품 순위'
-      queryKey='todayBest'
-      type={'work'}
-      fetchFn={getBestWorksApi}
-      variant='best'
-    />
+    <WorkSection title={isPathNameTitle} queryKey='todayBest' type={'work'} fetchFn={getBestWorksApi} variant='best' />
   );
 };
 
