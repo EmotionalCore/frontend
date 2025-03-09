24e387ab00b6430291ec767b74ec7471fa58b8da
SJ-1220 <kindjin12@naver.com>
2025-03-09 22:50:37 +0900

    Design: "SignIn UI 구현"

--- src/app/_components/domain/auth/SignInForm.tsx
+++ src/app/_components/domain/auth/SignInForm.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 import Inputs from '@/app/_components/_common/Inputs/Inputs';
 import { useForm, Controller } from 'react-hook-form';
 import { useMutation } from '@tanstack/react-query';
-import { JwtResponse, PostSignInProps } from '../../../../api/auth/type';
-import { postSignInApi } from '../../../../api/auth';
+import { JwtResponse, PostSignInProps } from '@/api/auth/type';
+import { postSignInApi } from '@/api/auth';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { InputsSignInValidation } from '@/app/_lib/zod/InputsValidation';
 import { useRouter } from 'next/navigation';
+import Buttons from '@/app/_components/_common/Buttons/Button';
 
 const SignInForm = () => {
   const router = useRouter();
@@ -44,39 +45,45 @@ const SignInForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col items-center justify-center gap-4 rounded-md p-6'>
-      <Controller
-        name='email'
-        control={control}
-        render={({ field }) => <Inputs {...field} type='email' label='이메일' helpText={undefined} errors={errors} />}
-      />
-      <Controller
-        name='password'
-        control={control}
-        render={({ field }) => (
-          <Inputs
-            {...field}
-            type='password'
-            label='비밀번호'
-            helpText={'영문 대, 소문자/숫자/특수문자 포함, 8~15자'}
-            errors={errors}
-          />
-        )}
-      />
-      <button
-        type='submit'
-        disabled={!isValid || signInMutation.isPending}
-        className='border-black w-full rounded-lg border-[0.1rem] border-solid p-2 transition-colors hover:bg-gray-100 disabled:cursor-not-allowed disabled:opacity-50'
+    <>
+      <div className='mt-[3rem] font-SCDream5 text-[2.8rem]'>로그인</div>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className='flex flex-col items-center justify-center gap-4 rounded-md p-6'
       >
-        {signInMutation.isPending ? 'Signing Up...' : 'Sign Up'}
-      </button>
-
-      {signInMutation.isError && (
-        <p>
-          {signInMutation.error instanceof Error ? signInMutation.error.message : 'An error occurred during sign up'}
-        </p>
-      )}
-    </form>
+        <Controller
+          name='email'
+          control={control}
+          render={({ field }) => <Inputs {...field} type='email' label='이메일' helpText={undefined} errors={errors} />}
+        />
+        <Controller
+          name='password'
+          control={control}
+          render={({ field }) => (
+            <Inputs
+              {...field}
+              type='password'
+              label='비밀번호'
+              helpText={'영문 대, 소문자/숫자/특수문자 포함, 8~15자'}
+              errors={errors}
+            />
+          )}
+        />
+        <Buttons
+          type='submit'
+          intent={!isValid || signInMutation.isPending ? 'gray' : 'skyblue'}
+          size='mdLogIn'
+          disabled={!isValid || signInMutation.isPending}
+        >
+          확인
+        </Buttons>
+        {signInMutation.isError &&
+          (() => {
+            console.log(signInMutation.error instanceof Error ? signInMutation.error.message : '로그인 중 에러 발생');
+            return null;
+          })()}
+      </form>
+    </>
   );
 };
 
