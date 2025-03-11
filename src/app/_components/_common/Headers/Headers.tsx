2ee3b4587c8ef10a4a532e399992b80333b86010
SJ-1220 <kindjin12@naver.com>
2025-03-11 16:53:07 +0900

    Refactor: "이미지 버튼 컴포넌트화, hover 디자인 적용, 링크 따로 정의"

--- src/app/_components/_common/Headers/Headers.tsx
+++ src/app/_components/_common/Headers/Headers.tsx
@@ -16,17 +16,30 @@ interface LinksProps {
   href: string;
   label: string;
   isActive: boolean;
-  marginLeft: keyof typeof marginOption;
+  marginLeft: keyof typeof linksStlyeOption;
 }
 
-const marginOption = {
-  '4.9': 'ml-[4.9rem] md:ml-[3rem]',
-  '3.8': 'ml-[3.8rem] md:ml-[3rem]',
+interface ImageLinksProps {
+  src: string;
+  alt: string;
+  style: keyof typeof imageButtonStyleOption;
+}
+
+const linksStlyeOption = {
+  '4.9': 'ml-[4.9rem] md:ml-[3rem] hover:text-black-2',
+  '3.8': 'ml-[3.8rem] md:ml-[3rem] hover:text-black-2',
+};
+
+const imageButtonStyleOption = {
+  logo: 'relative mr-[0.7rem] h-[3.6355rem] w-[4.7564rem]',
+  title: 'relative h-[2.2272rem] w-[8.7453rem] sm:hidden',
+  add: 'relative size-[2.4rem]',
+  user: 'relative flex size-[3.6rem] items-center',
 };
 
 const Links = ({ href, label, isActive, marginLeft }: LinksProps) => {
   return (
-    <div className={`${marginOption[marginLeft] || ''} ${isActive ? 'text-black-2' : 'text-gray-9'}`}>
+    <div className={`${linksStlyeOption[marginLeft] || ''} ${isActive ? 'text-black-2' : 'text-gray-9'}`}>
       <Link href={href} passHref>
         {label}
       </Link>
@@ -34,37 +47,46 @@ const Links = ({ href, label, isActive, marginLeft }: LinksProps) => {
   );
 };
 
+const ImageButton = ({ src, alt, style }: ImageLinksProps) => {
+  return (
+    <div className={`${imageButtonStyleOption[style] || ''}`} title={alt}>
+      <Image src={src} alt={alt} fill style={{ objectFit: 'contain' }} />
+    </div>
+  );
+};
+
+const linksData = {
+  home: '/',
+  board: '/board',
+  search: '/search',
+  library: '/library',
+  post: '/forum/post',
+  signup: '/signup',
+};
+
 const Headers = ({ isMainPage, isBoardPage, isSearchPage, isLibraryPage }: HeadersProps) => {
   return (
     <header className='m-auto flex h-[6.3rem] flex-row items-center justify-between bg-white-F font-SCDream5 text-[1.8rem] text-black-2 sm:h-[9.6rem] sm:w-[37.3rem] sm:flex-col md:w-[74.4rem] lg:w-[120rem]'>
       {/* desktop/tablet 감성코어 홈 게시판 검색 서재 */}
       <div className='flex-low flex items-center sm:flex-col'>
-        <div className='relative mr-[0.7rem] h-[3.6355rem] w-[4.7564rem]'>
-          <Image src={LogoImage} alt='logo' fill style={{ objectFit: 'contain' }} />
-        </div>
-        <div className='relative h-[2.2272rem] w-[8.7453rem] sm:hidden'>
-          <Image src={TitleImage} alt='title' fill style={{ objectFit: 'contain' }} />
-        </div>
-        <Links href='/' marginLeft='4.9' label='홈' isActive={isMainPage} />
-        <Links href='/board' marginLeft='3.8' label='게시판' isActive={isBoardPage} />
-        <Links href='/search' marginLeft='3.8' label='검색' isActive={isSearchPage} />
-        <Links href='/library' marginLeft='3.8' label='서재' isActive={isLibraryPage} />
+        <ImageButton src={LogoImage} alt='logo' style='logo' />
+        <ImageButton src={TitleImage} alt='title' style='title' />
+        <Links href={linksData.home} marginLeft='4.9' label='홈' isActive={isMainPage} />
+        <Links href={linksData.board} marginLeft='3.8' label='게시판' isActive={isBoardPage} />
+        <Links href={linksData.search} marginLeft='3.8' label='검색' isActive={isSearchPage} />
+        <Links href={linksData.library} marginLeft='3.8' label='서재' isActive={isLibraryPage} />
       </div>
       {/* desktop/tablet 작품등록 프로필 */}
       <div className='flex-low flex sm:flex-col'>
-        <Link href='/forum/post' passHref>
+        <Link href={linksData.post} passHref>
           <div className='flex items-center'>
-            <div className='relative size-[2.4rem]'>
-              <Image src={AddIcon} alt='add icon' fill style={{ objectFit: 'contain' }} />
-            </div>
+            <ImageButton src={AddIcon} alt='add' style='add' />
             <div className='ml-[0.6rem] text-[2rem] text-blue-0'>작품등록</div>
           </div>
         </Link>
         <div className='ml-[4.25rem] md:ml-[3.3rem]'>
-          <Link href='/signup' passHref>
-            <div className='relative flex size-[3.6rem] items-center' title='none_user'>
-              <Image src={NoneUserIcon} alt='none user icon' fill style={{ objectFit: 'contain' }} />
-            </div>
+          <Link href={linksData.signup} passHref>
+            <ImageButton src={NoneUserIcon} alt='none user' style='user' />
           </Link>
         </div>
       </div>
