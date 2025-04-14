de4cb68386f17df4d28048d5bda279545fc28f42
박지원 <jackgg12322@gmail.com>
2025-04-14 23:01:08 +0900

    Setting:"board 폴더 -> forum 폴더 명 수정 및 불필요 파일 제거"

--- src/app/(sub pages)/forum/works/detail/[id]/page.tsx
+++ src/app/(sub pages)/forum/works/detail/[id]/page.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 interface WorkDetailProps {
   params: { id: string };
 }
-const Detail = ({ params }: WorkDetailProps) => {
+const page = ({ params }: WorkDetailProps) => {
   return <div>id : {params.id}</div>;
 };
 
-export default Detail;
+export default page;
