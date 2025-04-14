d76ca8f6801a85a275d7914d7db8588a0d1af05f
박지원 <jackgg12322@gmail.com>
2025-04-15 00:38:31 +0900

    Fix:"Error occurred pre rendering page 에러 해결 시도(ReactEditor.tsx)"

--- src/app/_components/domain/forum/form/ReactEditor.tsx
+++ src/app/_components/domain/forum/form/ReactEditor.tsx
@@ -1,10 +1,15 @@
 'use client';
 
 import { useMemo, useState } from 'react';
-import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import ReactModule from './ReactModule';
 import dompurify from 'dompurify';
+import dynamic from 'next/dynamic';
+import Loading from '@/app/loading';
+const ReactQuill = dynamic(() => import('react-quill'), {
+  loading: () => <Loading type='spinner' />,
+  ssr: false,
+});
 const ReactEditor = () => {
   const formats: string[] = [
     'header',
