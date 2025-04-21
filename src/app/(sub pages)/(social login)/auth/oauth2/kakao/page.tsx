ea2876c0f75090393d89eb3c4ea23fb3ffa74ec2
SJ-1220 <kindjin12@naver.com>
2025-04-21 17:36:08 +0900

    Feat: "카카오 로그인 구현 완료"

--- src/app/(sub pages)/(social login)/auth/oauth2/kakao/page.tsx
+++ src/app/(sub pages)/(social login)/auth/oauth2/kakao/page.tsx
@@ -1,23 +1,44 @@
 'use client';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
+import Loading from '@/app/loading';
 
 const KakaoPage = () => {
   const searchParams = useSearchParams();
   const router = useRouter();
 
+  const kakaoCallRef = useRef(false);
   useEffect(() => {
-    const accessToken = searchParams.get('accessToken');
-    const refreshToken = searchParams.get('refreshToken');
+    const code = searchParams.get('code');
+    // console.log('인가코드 Kakao: ', code);
 
-    if (accessToken && refreshToken) {
-      localStorage.setItem('AccessToken', accessToken);
-      localStorage.setItem('RefreshToken', refreshToken);
-      router.push('/');
-    }
+    if (!code || kakaoCallRef.current) return;
+    kakaoCallRef.current = true;
+
+    const getTokens = async () => {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/oauth2/kakao`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ code }),
+      });
+
+      if (res.ok) {
+        const data = await res.json();
+        localStorage.setItem('AccessToken', data.accessToken);
+        localStorage.setItem('RefreshToken', data.refreshToken);
+        router.push('/');
+      } else {
+        console.error('토큰 요청 실패');
+      }
+    };
+    getTokens();
   }, [searchParams, router]);
 
-  return <div>카카오 로그인 처리 중입니다...</div>;
+  return (
+    <div>
+      <Loading type='spinner' />
+    </div>
+  );
 };
 
 export default KakaoPage;
