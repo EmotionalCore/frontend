323b1c6aa24339329c2edc716a6be84f04655cfb
박지원 <jackgg12322@gmail.com>
2025-04-21 23:53:06 +0900

    Feat:"등록 페이지 분야 드롭다운 메뉴 컴포넌트 및 커스텀 훅 개발"

---
+++ src/app/_components/domain/forum/form/selectBox/ClassSeriesDropDown.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { Dispatch, SetStateAction } from 'react';
+
+interface ClassSeriesDropDownProps {
+  id: number;
+  key: number;
+  value: string;
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+  setSeriesTypeValue: (value: string) => void;
+}
+
+const ClassSeriesDropDown = ({ isOpen, setIsOpen, id, value, setSeriesTypeValue }: ClassSeriesDropDownProps) => {
+  const onSelectSeriesValue = () => {
+    setSeriesTypeValue(value);
+    setIsOpen(!isOpen);
+  };
+  return (
+    <li
+      className='flex w-[30.9rem] cursor-pointer flex-col justify-center border-b-[0.1rem] border-gray-E hover:bg-gray-50'
+      onClick={onSelectSeriesValue}
+      key={id}
+    >
+      <p className='py-[3rem] pb-[2rem] font-SCDream1 text-18-500 text-black-2'>{value}</p>
+    </li>
+  );
+};
+
+export default ClassSeriesDropDown;
