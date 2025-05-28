c9756789a1988164f0016fd942d4022bc0fa0739
SJ-1220 <kindjin12@naver.com>
2025-05-28 18:38:53 +0900

    Feat: 마이페이지 로그아웃, 탈퇴 기능 구현

--- src/app/_components/domain/user/DeleteAccount.tsx
+++ src/app/_components/domain/user/DeleteAccount.tsx
@@ -1,4 +1,29 @@
+'use client';
+import { deleteAccoutApi } from '@/api/mypage';
+import { useRouter } from 'next/navigation';
+import Buttons from '../../_common/Buttons/Button';
+
 const DeleteAccount = () => {
-  return <div>탈퇴</div>;
+  const router = useRouter();
+
+  const DeleteAccountButton = async () => {
+    try {
+      await deleteAccoutApi();
+      localStorage.removeItem('accessToken');
+      console.log('정상적으로 탈퇴');
+      router.push('/');
+    } catch (error) {
+      console.error('탈퇴 중 에러:', error);
+    }
+  };
+  return (
+    <div>
+      <div>탈퇴하기</div>
+      <div>회원 탈퇴 안내사항</div>
+      <div>안녕하세요, 감성코어 운영팀입니다.</div>
+      <Buttons onClick={DeleteAccountButton}>탈퇴하기</Buttons>
+    </div>
+  );
 };
+
 export default DeleteAccount;
